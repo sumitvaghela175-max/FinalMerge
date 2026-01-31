f8c40f5c1ea5d44930e8e53f727d8d3559cfcc09
jadeja kuldeepsinh <kpjadeja1304@gmail.com>
2026-01-31 14:30:05 +0530

    Add function to determine largest of three numbers
    Implement program to find the largest of three numbers using if statements.

--- FindMaxFinal.py
+++ FindMaxFinal.py
@@ -1,1 +1,14 @@
 #Find both half of the programs from the other two repositories and connect them together to make a program
+
+#WAP to print the largest number between 3 using if
+
+n1 = int(input("enter the first number: "))
+n2 = int(input("enter the second number: "))
+n3 = int(input("enter the third number: "))
+
+if n1 > n2 and n1 > n3:
+    print(f"{n1} is the largest")
+elif n2 > n1 and n2 > n3:
+    print(f"{n2} is the largest")
+else:
+    print(f"{n3} is the largest")
